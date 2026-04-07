c08c1dccd80d804b96180fb5c85bb863db759264
Henrique Morilha <henriquemorilha97@gmail.com>
2026-04-07 04:23:07 -0300

    Add root endpoint and dynamic port

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def status():
         "status": random.choice(["SUCCESS", "FAIL", "RUNNING"])
     })
 
-# 👉 Porta dinâmica (pro Render)
+# Porta dinâmica (pro Render)
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
